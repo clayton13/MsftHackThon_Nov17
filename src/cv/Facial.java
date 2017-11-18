06fc157eb02ca1b169288b953c74cd083a0edde6
Clayton White <clwht687@gmail.com>
2017-11-17 22:00:17 -0500

    add facial

---
+++ src/cv/Facial.java
@@ -0,0 +1,16 @@
+package cv;
+
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+public class Facial {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+	}
+
+
+}
